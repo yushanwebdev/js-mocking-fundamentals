58892e03c716450b8995bb5a5392ba4af98c170b
yushan <yushanwebdev@gmail.com>
2022-05-01 13:45:52 +0530

    feat: Override Object Properties to Mock with Monkey-patching in JavaScript

--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -1,12 +1,11 @@
-const assert = require('assert')
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const assert = require("assert");
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
 
-const originalGetWinner = utils.getWinner
-utils.getWinner = (p1, p2) => p1
+const originalGetWinner = utils.getWinner;
+utils.getWinner = (p1, p2) => p1;
 
-const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-assert.strictEqual(winner, 'Kent C. Dodds')
+const winner = thumbWar("Kent C Dodds", "Ken Wheeler");
+assert.strictEqual(winner, "Kent C Dodds");
 
-// cleanup
-utils.getWinner = originalGetWinner
+utils.getWinner = originalGetWinner;
